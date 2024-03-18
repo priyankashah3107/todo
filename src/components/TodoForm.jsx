d07cca1b0eef457bfdcfad3ac2967c8c1452b51e
Priyanka <priyankashah3107@gmail.com>
2024-03-18 23:44:26 +0530

    adding States

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import { useTodo } from "../context";
+
 function TodoForm() {
-    
+    const [todo, setTodo] = useState("")
+    const {addTodo} = useTodo()
+
+    const Add = (ev) => {
+         ev.preventDefault();
+         
+         if(!todo) return;
+
+         setTodo({todo, completed: false})
+         setTodo("")
+    }
 
   return (
-      <form  className="flex">
+      <form  onSubmit={Add} className="flex">
           <input
               type="text"
               placeholder="Write Todo..."
               className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+              value={todo}  // doing wiring
+              onChange={(e) => setTodo(e.target.value)}
           />
           <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
               Add
